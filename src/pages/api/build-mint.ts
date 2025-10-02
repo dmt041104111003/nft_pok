438f1fcb5209f0d24810e8b123a30826991c4e1f
dmt041104111003 <daomanhtung4102003@gmail.com>
2025-10-02 20:39:35 +0700

    feat: apply Params

--- src/pages/api/build-mint.ts
+++ src/pages/api/build-mint.ts
@@ -1,8 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import fs from 'fs/promises'
 import path from 'path'
-import { BlockfrostProvider, MeshTxBuilder, applyCborEncoding, resolveScriptHash, deserializeAddress, serializePlutusScript } from '@meshsdk/core'
-import child_process from 'child_process'
+import { BlockfrostProvider, MeshTxBuilder, applyParamsToScript, resolveScriptHash, deserializeAddress, serializePlutusScript } from '@meshsdk/core'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
@@ -15,41 +14,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     const provider = new BlockfrostProvider(process.env.BLOCKFROST_KEY!)
 
-    const smcDir = path.join(process.cwd(), 'smc')
     const publicBlueprintPath = path.join(process.cwd(), 'public', 'plutus.json')
-    const smcBlueprintPath = path.join(smcDir, 'plutus.json')
-
-    const getVaultHash = async () => {
-      const readJson = async (p: string) => {
-        try { return JSON.parse(await fs.readFile(p, 'utf8')) } catch { return null }
-      }
-      const fromPublic = await readJson(publicBlueprintPath)
-      const fromSmc = await readJson(smcBlueprintPath)
-      const pick = (bp: any) => bp?.validators?.find((v: any) => v.title?.includes('vault.spend'))?.hash
-      return pick(fromPublic) || pick(fromSmc) || ''
-    }
-
-    const ownerHash = deserializeAddress(address).pubKeyHash
-    const vaultHash = await getVaultHash()
-    if (ownerHash && vaultHash) {
-      const envPath = path.join(smcDir, 'env', 'default.ak')
-      const libPath = path.join(smcDir, 'lib', 'config.ak')
-      const updateCfg = async (p: string) => {
-        try {
-          let txt = await fs.readFile(p, 'utf8')
-          txt = txt.replace(/pub const owner:[^=]*=\s*#"[0-9a-fA-F]{56}"/g, `pub const owner: VerificationKeyHash = #"${ownerHash}"`)
-          txt = txt.replace(/pub const vault:[^=]*=\s*Script\(#"[0-9a-fA-F]{56}"\)/g, `pub const vault: Credential = Script(#"${vaultHash}")`)
-          await fs.writeFile(p, txt, 'utf8')
-        } catch {}
-      }
-      await updateCfg(envPath)
-      await updateCfg(libPath)
-      try {
-        child_process.execSync('aiken build', { cwd: smcDir })
-        await fs.copyFile(smcBlueprintPath, publicBlueprintPath)
-      } catch {}
-    }
-
     const blueprintPath = publicBlueprintPath
     const blueprint = JSON.parse(await fs.readFile(blueprintPath, 'utf8'))
     const nftPolicy = blueprint.validators.find((v: any) => v.title.includes('nft_policy.mint'))
@@ -57,7 +22,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const vaultVal = blueprint.validators.find((v: any) => v.title.includes('vault.spend'))
     if (!vaultVal) return res.status(500).json({ error: 'vault.spend not found in plutus.json' })
 
-    const scriptCbor = applyCborEncoding(nftPolicy.compiledCode)
+    const ownerHash = deserializeAddress(address).pubKeyHash
+    const scriptCbor = applyParamsToScript(nftPolicy.compiledCode, [ownerHash])
     const policyId = resolveScriptHash(scriptCbor, 'V3')
     const assetName = 'NFT'
     const assetHex = Buffer.from(assetName, 'utf8').toString('hex')
@@ -95,7 +61,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       const md: any = { [policyId]: { [assetName]: { name: name || 'NFT', image: image || '' } } }
       builder.metadataValue('721', md)
     }
-    const vaultCbor = applyCborEncoding(vaultVal.compiledCode)
+    const vaultCbor = applyParamsToScript(vaultVal.compiledCode, [ownerHash, policyId, assetHex])
     const vaultAddress = serializePlutusScript({ code: vaultCbor, version: 'V3' }, undefined, 0).address
     builder.txOut(vaultAddress, [{ unit, quantity: '1' }])
 
